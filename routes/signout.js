469ac68ee9903ffa365cbe44926c4b994597dab0
Ivan <bfg546@yandex.ru>
2022-01-12 12:11:22 +0300

    Fix clearCookie

--- routes/signout.js
+++ routes/signout.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.status(200).clearCookie('jwt').send({ message: 'Выход' });
+  res.status(200).clearCookie('jwt', {
+    sameSite: 'none',
+  }).send({ message: 'Выход' });
 });
 
 module.exports = router;
